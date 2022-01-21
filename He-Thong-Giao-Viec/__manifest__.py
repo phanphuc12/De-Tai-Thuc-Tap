3797c28d19bf9e19f8679c45d6df512f06f2b560
phantenphuc12 <baophuc12k@gmail.com>
2022-01-21 16:46:52 +0700

    Create __manifest__.py

---
+++ He-Thong-Giao-Viec/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Assignment System',
+    'version': '1.0',
+    'summary': 'He Thog Giao Viec',
+    'sequence': -10,
+    'description': """He Thong Giao Viec""",
+    'category': 'Human Resources',
+    'license': 'LGPL-3',
+    'depends': [
+        'hr'
+    ],
+    'data': [
+        'Views/NhanVien.xml',
+        'Menu/Menu.xml'
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
