dd4ae00f88b28242b6f5f92dde3d407d0f71090b
phantenphuc12 <baophuc12k@gmail.com>
2022-02-14 10:59:17 +0700

    Update assignment.py

--- He-Thong-Giao-Viec/Models/assignment.py
+++ He-Thong-Giao-Viec/Models/assignment.py
@@ -8,8 +8,16 @@ class assignment(models.Model):
     department = fields.Many2one('hr.department', string="Department", required=True)
     employee = fields.Many2one('hr.employee', string='Employee', required=True)
     deadline = fields.Datetime(string='Deadline', required=True)
+    description = fields.Text(string='Description')
     name_seq = fields.Char(string='ID', required=True, copy=False, readonly=True, index=True,
                            default=lambda self: _('New'))
+    status = fields.Selection(
+        [('draft', 'Draft'),
+         ('send', 'Send'),
+         ('completed', 'Completed'),
+         ('confirmed', 'Confirmed')
+         ], string='Status', default='draft', readonly=True
+    )
 
     @api.model
     def create(self, vals):
@@ -22,3 +30,14 @@ class assignment(models.Model):
     def related_department(self):
         for rec in self:
             return {'domain': {'employee': [('department_id', '=', rec.department.id)]}}
+
+    def send_assignment(self):
+        vals = {
+            'description': self.description,
+            'department': self.department.id,
+            'employee': self.employee.id,
+            'name_seq': self.name_seq,
+            'deadline': self.deadline,
+
+        }
+        self.env['my.assignment'].create(vals)
