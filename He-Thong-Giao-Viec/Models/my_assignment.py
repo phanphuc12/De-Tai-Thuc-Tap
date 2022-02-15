2f39e946bd3ac74350a32c789ac116c732b9f979
phantenphuc12 <baophuc12k@gmail.com>
2022-02-15 08:38:55 +0700

    Update my_assignment.py

--- He-Thong-Giao-Viec/Models/my_assignment.py
+++ He-Thong-Giao-Viec/Models/my_assignment.py
@@ -11,3 +11,23 @@ class MyAssignment(models.Model):
     description = fields.Text(string='Description')
     name_seq = fields.Char(string='ID', required=True, copy=False, readonly=True, index=True,
                            default=lambda self: _('New'))
+    state = fields.Selection(
+        [('received', 'Received'),
+         ('complete', 'Completed'),
+         ('confirm', 'Confirmed')
+         ], string='Status', default='received', readonly=True)
+
+    def action_complete(self):
+        for rec in self:
+            rec.state = 'complete'
+            ma = self.env['assignment.s'].search([('name_seq', '=', rec.name_seq)])
+            if ma.name_seq:
+                ma.state = "complete"
+                # print('check...', ma.state)
+
+    # @api.onchange('state')
+    # def state_complete(self):
+    #     for rec in self:
+    #         ma = self.env['assignment.s'].search([('name_seq', '=', rec.name_seq)])
+    #         if ma:
+    #             print("test...", ma.name_seq)
