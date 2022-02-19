ece9b0002f85d30edffd8972ad6818d1949b2ebb
phantenphuc12 <baophuc12k@gmail.com>
2022-02-19 11:31:56 +0700

    Create projects.py

---
+++ He-Thong-Giao-Viec/Models/projects.py
@@ -0,0 +1,27 @@
+from odoo import api, fields, models, _
+
+
+class Projects(models.Model):
+    _name = "project.s"
+    _inherit = ["mail.thread"]
+
+    name = fields.Char(string='ID', required=True, copy=False, readonly=True, index=True,
+                       default=lambda self: _('New'))
+    project = fields.Char(string="Project Name", required=True)
+    department = fields.Many2many('hr.department', string='Department', required=True)
+    employee = fields.Many2one('hr.employee', string='PM Name', required=True)
+    start_date = fields.Date(string="Start Date", requied=True)
+    description = fields.Text(string="Description")
+
+    @api.model
+    def create(self, vals):
+        if vals.get('name', _('New')) == _('New'):
+            vals['name'] = self.env['ir.sequence'].next_by_code('project.s.sequence') or _('New')
+        result = super(Projects, self).create(vals)
+        return result
+
+    @api.onchange('department')
+    def related_department(self):
+        for rec in self:
+            # print('print:....', rec.current_user)
+            return {'domain': {'employee': [('department_id', '=', rec.department.id)]}}
