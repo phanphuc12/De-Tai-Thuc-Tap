ebe336775bb51ce7dac85bfdc9afa2c9796373a9
phantenphuc12 <baophuc12k@gmail.com>
2022-02-19 11:31:49 +0700

    Update assignment.py

--- He-Thong-Giao-Viec/Models/assignment.py
+++ He-Thong-Giao-Viec/Models/assignment.py
@@ -5,13 +5,14 @@ class assignment(models.Model):
     _name = "assignment.s"
     _inherit = ["mail.thread"]
 
-    name = fields.Char("name")
+    name = fields.Char(string='ID', required=True, copy=False, readonly=True, index=True,
+                           default=lambda self: _('New'))
     department = fields.Many2one('hr.department', string="Department", required=True)
     employee = fields.Many2one('res.users', string='Employee', required=True)
     deadline = fields.Datetime(string='Deadline', required=True)
     description = fields.Text(string='Description')
-    name_seq = fields.Char(string='ID', required=True, copy=False, readonly=True, index=True,
-                           default=lambda self: _('New'))
+    # name_seq = fields.Char(string='ID', required=True, copy=False, readonly=True, index=True,
+    #                        default=lambda self: _('New'))
     state = fields.Selection(
         [('draft', 'Draft'),
          ('send', 'Sent'),
@@ -27,7 +28,7 @@ class assignment(models.Model):
             'description': self.description,
             'department': self.department.id,
             'employee': self.employee.id,
-            'name_seq': self.name_seq,
+            # 'name_seq': self.name_seq,
             'deadline': self.deadline,
 
         }
@@ -39,14 +40,14 @@ class assignment(models.Model):
     def action_confirm(self):
         for rec in self:
             rec.state = 'confirm'
-            ma = self.env['my.assignment'].search([('name_seq', '=', rec.name_seq)])
-            if ma.name_seq:
+            ma = self.env['my.assignment'].search([('name', '=', rec.name)])
+            if ma.name:
                 ma.state = "confirm"
 
     @api.model
     def create(self, vals):
-        if vals.get('name_seq', _('New')) == _('New'):
-            vals['name_seq'] = self.env['ir.sequence'].next_by_code('assignment.s.sequence') or _('New')
+        if vals.get('name', _('New')) == _('New'):
+            vals['name'] = self.env['ir.sequence'].next_by_code('assignment.s.sequence') or _('New')
         result = super(assignment, self).create(vals)
         return result
 
