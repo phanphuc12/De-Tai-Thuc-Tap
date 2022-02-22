3602078c97e084bf8930cc800a67d62c290ce7de
phantenphuc12 <baophuc12k@gmail.com>
2022-02-22 14:14:48 +0700

    Update my_assignment.py

--- He-Thong-Giao-Viec/Models/my_assignment.py
+++ He-Thong-Giao-Viec/Models/my_assignment.py
@@ -7,19 +7,25 @@ class MyAssignment(models.Model):
     _description = "My Assignment"
 
     name = fields.Char(string='ID', required=True, copy=False, readonly=True, index=True,
-                           default=lambda self: _('New'))
+                       default=lambda self: _('New'))
     department = fields.Many2one('hr.department', string="Department", required=True)
     employee = fields.Many2one('hr.employee', string='Employee', required=True)
     deadline = fields.Datetime(string='Deadline', required=True)
     description = fields.Text(string='Description')
-    # name_seq = fields.Char(string='ID', required=True, copy=False, readonly=True, index=True,
-    #                        default=lambda self: _('New'))
     state = fields.Selection(
         [('received', 'Received'),
          ('complete', 'Completed'),
          ('confirm', 'Confirmed')
          ], string='Status', default='received', readonly=True, tracking=True)
-    current_user = fields.Many2one('res.users', 'Current User', default=lambda self: self.env.user, readonly=True)
+    current_user = fields.Many2one('res.users', 'Creator', default=lambda self: self.env.user, readonly=True)
+    project_id = fields.Many2one('project.s', string='Project')
+    project_right = fields.Boolean(string='In The Project')
+    name_pm = fields.Many2one('res.users', related='project_id.name_pm', string='PM Name')
+    start_date = fields.Date(string="Start Date", related='project_id.start_date')
+    file = fields.Binary(string='Attached Files')
+    file_name = fields.Char(string="File Name")
+    reply_file = fields.Binary(string='Attached Files', tracking=True)
+    reply_file_name = fields.Char(string='Reply File Name')
 
     def action_complete(self):
         for rec in self:
@@ -27,4 +33,7 @@ class MyAssignment(models.Model):
             ma = self.env['assignment.s'].search([('name', '=', rec.name)])
             if ma.name:
                 ma.state = "complete"
+                ma.reply_file = rec.reply_file
+                ma.reply_file_name = rec.reply_file_name
                 print('check...', ma.state)
+
