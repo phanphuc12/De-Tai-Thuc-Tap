e6cddc682939aac0cf0cad5823924a2031e34508
phantenphuc12 <baophuc12k@gmail.com>
2022-02-22 14:14:46 +0700

    Update assignment.py

--- He-Thong-Giao-Viec/Models/assignment.py
+++ He-Thong-Giao-Viec/Models/assignment.py
@@ -6,13 +6,11 @@ class assignment(models.Model):
     _inherit = ["mail.thread"]
 
     name = fields.Char(string='ID', required=True, copy=False, readonly=True, index=True,
-                           default=lambda self: _('New'))
+                       default=lambda self: _('New'))
     department = fields.Many2one('hr.department', string="Department", required=True)
     employee = fields.Many2one('res.users', string='Employee', required=True)
     deadline = fields.Datetime(string='Deadline', required=True)
     description = fields.Text(string='Description')
-    # name_seq = fields.Char(string='ID', required=True, copy=False, readonly=True, index=True,
-    #                        default=lambda self: _('New'))
     state = fields.Selection(
         [('draft', 'Draft'),
          ('send', 'Sent'),
@@ -20,7 +18,16 @@ class assignment(models.Model):
          ('confirm', 'Confirmed')
          ], string='Status', default='draft', readonly=True, tracking=True
     )
-    current_user = fields.Many2one('res.users', 'Current User', default=lambda self: self.env.user, readony=True)
+    current_user = fields.Many2one('res.users', string='Creator', default=lambda self: self.env.user, readony=True)
+    project_id = fields.Many2one('project.s', string='Project')
+    project_right = fields.Boolean(string='In The Project')
+    name_pm = fields.Many2one('res.users', related='project_id.name_pm', string='PM Name')
+    create_time = fields.Datetime("Create Time", default=lambda self: fields.datetime.now())
+    start_date = fields.Date(string="Start Date", related='project_id.start_date')
+    file = fields.Binary(string='Attached Files')
+    file_name = fields.Char(string="File Name")
+    reply_file = fields.Binary(string='Attached Files', tracking=True)
+    reply_file_name = fields.Char(string='Reply File Name')
 
     def send_assignment(self):
         vals = {
@@ -28,8 +35,13 @@ class assignment(models.Model):
             'description': self.description,
             'department': self.department.id,
             'employee': self.employee.id,
-            # 'name_seq': self.name_seq,
             'deadline': self.deadline,
+            'project_id': self.project_id.id,
+            'project_right': self.project_right,
+            'name_pm': self.name_pm,
+            'start_date': self.start_date,
+            'file': self.file,
+            'file_name': self.file_name,
 
         }
         for rec in self:
