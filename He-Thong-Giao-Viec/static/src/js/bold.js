a6790224768f5766b104516425562d8203a9e9bb
phantenphuc12 <baophuc12k@gmail.com>
2022-02-23 16:56:55 +0700

    Create bold.js

---
+++ He-Thong-Giao-Viec/static/src/js/bold.js
@@ -0,0 +1,17 @@
+odoo.define('assignment.bold', function (require) {
+    "use strict";
+    // import packages
+    var basic_fields = require('web.basic_fields');
+    var registry = require('web.field_registry');
+
+    // widget implementation
+    var BoldWidget = basic_fields.FieldChar.extend({
+        _renderReadonly: function () {
+            this._super();
+            var old_html_render = this.$el.html();
+            var new_html_render = '<b>' + old_html_render + '</b>'
+            this.$el.html(new_html_render);
+        },
+    });
+    registry.add('bold', BoldWidget); // add our "bold" widget to the widget registry
+});
