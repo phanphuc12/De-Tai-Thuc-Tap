473d03f6a0af783942efebde1363ec8611d4b3f9
phantenphuc12 <baophuc12k@gmail.com>
2022-02-24 16:04:59 +0700

    Update bold.js

--- He-Thong-Giao-Viec/static/src/js/bold.js
+++ He-Thong-Giao-Viec/static/src/js/bold.js
@@ -13,5 +13,15 @@ odoo.define('assignment.bold', function (require) {
             this.$el.html(new_html_render);
         },
     });
+
+    var ItalicWidget = basic_fields.FieldChar.extend({
+        _renderReadonly: function () {
+            this._super();
+            var old_html_render = this.$el.html();
+            var new_html_render = '<i>' + old_html_render + '</i>'
+            this.$el.html(new_html_render);
+        },
+    });
     registry.add('bold', BoldWidget); // add our "bold" widget to the widget registry
+    registry.add('italic', ItalicWidget);
 });
