ed801e671617c9768522ff8085a70e4d2dd3932d
phantenphuc12 <baophuc12k@gmail.com>
2022-03-04 10:42:59 +0700

    Update __init__.py

--- He-Thong-Giao-Viec/Models/__init__.py
+++ He-Thong-Giao-Viec/Models/__init__.py
@@ -2,6 +2,7 @@ from . import employees
 from . import department
 from . import assignment
 from . import my_assignment
-from. import users
+from . import users
 from . import projects
 from . import topic_categories
+from . import assistance
