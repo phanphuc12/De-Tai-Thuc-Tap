996e0635514692878c9195ae561d5803a589b49c
phantenphuc12 <baophuc12k@gmail.com>
2022-03-04 10:43:18 +0700

    Update topic_categories.py

--- He-Thong-Giao-Viec/Models/topic_categories.py
+++ He-Thong-Giao-Viec/Models/topic_categories.py
@@ -7,3 +7,7 @@ class TopicCategories(models.Model):
     name = fields.Char(string="Topic Name", required=True)
     department = fields.Many2one('hr.department', string="Department", required=True)
     description = fields.Text(string="Description")
+    type = fields.Selection([
+        ('1', 'Assignment'),
+        ('2', 'Assistance')
+    ], string="Type", default="1")
