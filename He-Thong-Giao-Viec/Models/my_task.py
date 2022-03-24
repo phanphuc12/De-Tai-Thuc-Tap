ab0097845139f5d7cbd788c7a3abe9f5b3b7138b
phantenphuc12 <78059032+phantenphuc12@users.noreply.github.com>
2022-03-24 15:08:29 +0700

    Create my_task.py

---
+++ He-Thong-Giao-Viec/Models/my_task.py
@@ -0,0 +1,50 @@
+from odoo import api, fields, models, _
+
+
+class MyTask(models.Model):
+    _name = "task.management"
+
+    name = fields.Char('Name', required=True)
+    start_date = fields.Date('Start Date', required=True)
+    deadline = fields.Date('End Date', required=True)
+    description = fields.Html('Description')
+    assignment = fields.Many2one('assignment.s', string='Related Work')
+    creator = fields.Many2one('hr.employee', default=lambda self: self.env.user.employee_id)
+    project_id = fields.Many2one('project.s', string='Project', related='assignment.project_id')
+    project_right = fields.Boolean(string='In The Project', related='assignment.project_right')
+    name_pm = fields.Many2one('hr.employee', string='PM Name', related='assignment.name_pm')
+    file = fields.Binary(string='Attached Files', related='assignment.file')
+    file_name = fields.Char(string="File Name", related='assignment.file_name')
+    topic = fields.Many2one('topic.category', string="Topic", required=True, related='assignment.topic')
+    state_id = fields.Selection([
+        ('todo', 'To Do'),
+        ('doing', 'Doing'),
+        ('done', 'Done')
+    ], default='todo', group_expand='_read_group_selection_field')
+    sprint = fields.Many2one('sprint.management', string='Sprint', required=True)
+    color = fields.Integer('Color Index', compute="set_kanban_color")
+    priority = fields.Selection([
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    ], default='low', string='Priority', group_expand='_read_group_selection_priority')
+
+    def set_kanban_color(self):
+        for record in self:
+            if record.priority == 'low':
+                color = 0
+            elif record.priority == 'medium':
+                color = 2
+            elif record.priority == 'high':
+                color = 1
+            else:
+                color = 1
+            record.color = color
+
+    @api.model
+    def _read_group_selection_field(self, values, domain, order):
+        return ['todo', 'doing', 'done']
+
+    @api.model
+    def _read_group_selection_priority(self, values, domain, order):
+        return ['low', 'medium', 'high']
